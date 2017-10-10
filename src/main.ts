14f0b9ebc0d7733e69e660813d40944d8576c2cf
Beatriz Magalhães <mbeatrizmagalhaes@gmail.com>
2017-10-10 22:57:47 +0100

    working with ts, webpack; with react and jest, to be explored

--- src/main.ts
+++ src/main.ts
@@ -1,77 +1,18 @@
-import * as electron from 'electron';
-import * as store from 'electron-json-storage'
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+import { app, BrowserWindow } from 'electron'
+declare var __dirname: string
+let mainWindow: Electron.BrowserWindow
 
-const path = require('path')
-const url = require('url')
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
-
-const userDataPath = (electron.app || electron.remote.app).getPath('userData')
-store.setDataPath(path.join(userDataPath, 'user-preferences'))
-
-function createWindow () {
-  let width, height
-  store.get('windowBounds', (err, data) => {
-    if (err) throw err;
-    ({ width, height } = data)
-
-    console.log(`w: ${width}, h: ${height}`)
-
-    // Create the browser window.
-    mainWindow = new BrowserWindow({width, height})
-
-    mainWindow.on('resize', () => {
-      let { width, height } = mainWindow.getBounds()
-      store.set('windowBounds', { width, height }, (error) => { if (error) throw error })
-    })
-
-    // and load the index.html of the app.
-    mainWindow.loadURL(url.format({
-      pathname: path.join(__dirname, 'index.html'),
-      protocol: 'file:',
-      slashes: true
-    }))
-
-    // Open the DevTools.
-    // mainWindow.webContents.openDevTools()
-
-    // Emitted when the window is closed.
-    mainWindow.on('closed', function () {
-      // Dereference the window object, usually you would store windows
-      // in an array if your app supports multi windows, this is the time
-      // when you should delete the corresponding element.
-      mainWindow = null
-    })
+function onReady() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
   })
+
+  const fileName = `file://${__dirname}/index.html`
+  mainWindow.loadURL(fileName)
+  mainWindow.on('close', () => app.quit())
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-//   if (process.platform !== 'darwin') {
-  app.quit()
-//   }
-})
-
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  // if (mainWindow === null) {
-  //   createWindow()
-  // }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+app.on('ready', () => onReady())
+app.on('window-all-closed', () => app.quit())
+console.log(`Electron Version ${app.getVersion()}`)

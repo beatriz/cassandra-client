b7155572ed8d84ab758b6570c3e1f3c1cf4cb063
Beatriz Magalhaes <mbeatrizmagalhaes@gmail.com>
2017-11-28 14:31:30 +0000

    put the connection form inside a modal

--- src/renderer/components/Connection.vue
+++ src/renderer/components/Connection.vue
@@ -1,17 +1,29 @@
 <template>
-  <div class="hello">
-    <label>Contact points</label>
-    <input type="text" v-model="contactPoints" />
-    <label>Port</label>
-    <input type="text" v-model="port" />
-    <button v-on:click="connect">Connect</button>
+  <div class="connection">
+    <button v-on:click="openModal" type="button">Connect</button>
 
-    <div v-bind:class="[textClass]">{{ status }}</div>
+    <div id="connection-modal" uk-modal>
+      <div class="uk-modal-dialog uk-modal-body"> 
+        <label>Contact points</label>
+        <input type="text" v-model="contactPoints" />
+        <label>Port</label>
+        <input type="text" v-model="port" />
+
+        <div v-bind:class="[textClass]">{{ status }}</div>
+
+        <p class="uk-text-right">
+            <button class="uk-button uk-button-default uk-modal-close" type="button">Cancel</button>
+            <button class="uk-button uk-button-primary" type="button" v-on:click="connect">Connect</button>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import * as cassandra from '../cassandra'
+const cassandra = require('../cassandra')
+const UIKit = require('uikit')
+
 export default {
   name: 'sidebar',
   data () {
@@ -31,11 +43,15 @@ export default {
         .then(() => {
           this.status = 'Connected'
           this.textClass = 'uk-text-success'
+          UIKit.modal('#connection-modal').hide()
         })
         .catch(err => {
           this.textClass = 'uk-text-danger'
           this.status = err.message
         })
+    },
+    openModal () {
+      UIKit.modal('#connection-modal').show()
     }
   }
 }

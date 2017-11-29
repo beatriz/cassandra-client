e74cfd3e1d3cad25e355e32afcb44e7680ea1466
Beatriz Magalhaes <mbeatrizmagalhaes@gmail.com>
2017-11-29 15:55:56 +0000

    treeview with keyspaces and fetching tables on click

--- src/renderer/cassandra.js
+++ src/renderer/cassandra.js
@@ -1,11 +1,11 @@
 const driver = require('cassandra-driver')
 
-let connection
+let client
 
 export function connect (contactPoints, port) {
   return new Promise((resolve, reject) => {
-    connection = new driver.Client({ contactPoints: contactPoints.split(','), protocolOptions: { port: port } })
-    connection.connect(err => {
+    client = new driver.Client({ contactPoints: contactPoints.split(','), protocolOptions: { port: port } })
+    client.connect(err => {
       if (err) reject(err)
       resolve()
     })
@@ -13,5 +13,16 @@ export function connect (contactPoints, port) {
 }
 
 export function getKeyspaces () {
-  return connection.execute('select keyspace_name from system_schema.keyspaces')
+  client.keyspace = 'system_schema'
+  return client.execute('select keyspace_name from keyspaces')
+}
+
+export function getKeyspaceInfo (keyspace) {
+  const query = 'select keyspace_name, table_name from tables where keyspace_name = ?'
+  return executePreparedQuery('system_schema', query, [keyspace])
+}
+
+function executePreparedQuery (keyspace, query, params) {
+  client.keyspace = keyspace
+  return client.execute(query, params, {prepare: true})
 }
--- src/renderer/components/Sidebar.vue
+++ src/renderer/components/Sidebar.vue
@@ -1,13 +1,23 @@
 <template>
-  <div id="sidebar">
+  <div id="sidebar" class="uk-width-1-4">
     <connection v-on:connected="getKeyspaces"></connection>
 
-    {{keyspaces.length}}
+    <ul class="uk-nav-default uk-nav-parent-icon" uk-nav>
+        <li class="uk-parent" v-for="k in keyspaces" :key="k.keyscpace_name">
+            <a href="#" v-on:click="keyspaceInfo(k.keyspace_name)">{{k.keyspace_name}}</a>
+            <ul class="uk-nav-sub" uk-nav>
+                <li v-for="table in k.tables" :key="table.table_name">
+                  <a href="#">{{table.table_name}}</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
   </div>
 </template>
 
 <script>
 import Connection from '@/components/Connection'
+import Vue from 'vue'
 const cassandra = require('../cassandra')
 
 export default {
@@ -24,6 +34,13 @@ export default {
     getKeyspaces () {
       cassandra.getKeyspaces()
         .then(res => { this.keyspaces = res.rows })
+        .catch(err => console.log(err))
+    },
+    keyspaceInfo (keyspaceName) {
+      cassandra.getKeyspaceInfo(keyspaceName)
+        .then(res => {
+          Vue.set(this.keyspaces, this.keyspaces.findIndex(x => x.keyspace_name === keyspaceName), {keyspace_name: keyspaceName, tables: res.rows})
+        })
         .catch(err => console.log(err))
     }
   }
